63666d5eddc12c715cf047fff0a3667fbfab7fc6
AnnaGulstine <annagulstine@gmail.com>
2016-03-23 21:09:02 -0500

    Added has_many and belongs_to associations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ActiveRecord::Base
   belongs_to :user
-  belongs_to :product
+  has_many :carted_products
+  has_many :products, through: :carted_products
 
   def friendly_created_at
     created_at.strftime("%A, %d %b %Y %l:%M %p")    

7dd72a93b84d3b0f98b650442554cb2f26737f06
AnnaGulstine <annagulstine@gmail.com>
2016-03-24 18:44:21 -0500

    Displayed carted products on carted products index page

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,2 +1,16 @@
 class CartedProductsController < ApplicationController
+  def index
+    @carted_products = CartedProduct.where("user_id = ? AND status = ?", current_user.id.to_i, "carted")
+  render 'index.html.erb'
+  end
+
+  def create
+    CartedProduct.create(
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      status: "carted"
+    )
+    redirect_to "/carted_products"
+  end
 end

fe54d75719e0f3fda787bf97e2126f8f34be6021
AnnaGulstine <annagulstine@gmail.com>
2016-05-02 16:53:24 -0500

    Added redirects for users who are not logged in

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,19 +1,28 @@
 class CartedProductsController < ApplicationController
   def index
-    @carted_products = CartedProduct.where("user_id = ? AND status = ?", current_user.id.to_i, "carted")
-    if @carted_products.length == 0
-      redirect_to '/products'
+    if current_user
+      @carted_products = CartedProduct.where("user_id = ? AND status = ?", current_user.id.to_i, "carted")
+      if @carted_products.length == 0
+        redirect_to '/products'
+      end
+    else
+      redirect_to '/'
     end
   end
 
   def create
-    CartedProduct.create(
-      user_id: current_user.id,
-      product_id: params[:product_id],
-      quantity: params[:quantity],
-      status: "carted"
-    )
-    redirect_to "/carted_products"
+    if current_user
+      CartedProduct.create(
+        user_id: current_user.id,
+        product_id: params[:product_id],
+        quantity: params[:quantity],
+        status: "carted"
+      )
+      redirect_to "/carted_products"
+    else
+      flash[:success] = "Please sign in to purchase products."      
+      redirect_to '/users/sign_in'
+    end
   end
 
   def destroy
